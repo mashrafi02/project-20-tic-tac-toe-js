8b74146423c0a177f6fc1811c1c72e0042514956
mashrafi <ahmedfirojmash365@gmail.com>
2025-04-14 18:17:20 +0600

    single player mode added

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const gameBoard = document.querySelector('.game-board')
-const playBtn = document.querySelector('.start');
+const playMulBtn = document.querySelector('.start-human');
+const playComBtn = document.querySelector('.start-computer');
 const resetBtn = document.querySelector('.reset');
+let playerStatus = document.querySelector('.status');
 
 let current_player = 'X';
 let board_values = ["","","","","","","","",""];
 let isActive;
-
-let playerStatus = document.querySelector('.status');
 
 const winningCombination = [
     [0,1,2],[3,4,5],
@@ -16,60 +16,154 @@ const winningCombination = [
 ]
 let winningCombo;
 
+// ########################################### Playing with Human start here ###################################################
+playMulBtn.onclick = ()=>{
+    playComBtn.style.display = 'none';
+    playMulBtn.style.display = 'none'
+    multiplayerHuman()
+}
 function multiplayerHuman(){
+    playerStatus.textContent = "Player X's turn"
+    isActive = true;
+    for (let i = 0; i < 9; i++){
+        gameBoard.innerHTML += `<div class="cell" data-index=${i}></div>`
+    }
+    playerStatus.style.display = 'block';
 
-    playBtn.onclick = () => {
-        playerStatus.textContent = "Player X's turn"
-        isActive = true;
-        for (let i = 0; i < 9; i++){
-            gameBoard.innerHTML += `<div class="cell" data-index=${i}></div>`
-        }
-        playerStatus.style.display = 'block';
-        playBtn.style.display = 'none'
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('click', ()=>{
+            index = parseInt(cell.getAttribute('data-index'))
+            if( isActive && board_values[index] == ""){
+                board_values[index] = current_player;
+                cell.textContent = current_player;
 
-        const cells = document.querySelectorAll('.cell');
-        cells.forEach(cell => {
-            cell.addEventListener('click', ()=>{
-                index = parseInt(cell.getAttribute('data-index'))
-                if( isActive && board_values[index] == ""){
-                    board_values[index] = current_player;
-                    cell.textContent = current_player;
-
-                    if (checkWinner()){
-                        winningCombo.forEach(combo => {
-                            cells[combo].classList.add('winning-cells')
-                        })
-                        drawLine(winningCombo, cells);
-                        playerStatus.textContent = `Player ${current_player} wins`;
-                        isActive = false;
-                        resetBtn.style.display = 'inline-block';
-                        return
-                    }
-
-                    if(checkDraw()){
-                        playerStatus.textContent = "It's a Draw";
-                        isActive = false;
-                        resetBtn.style.display = 'inline-block';
-                        return
-                    }
-                    current_player = current_player === 'X'? 'O' : 'X';
-                    playerStatus.textContent = `Player ${current_player}'s turn`;
+                if (checkWinner()){
+                    winningCombo.forEach(combo => {
+                        cells[combo].classList.add('winning-cells')
+                    })
+                    drawLine(winningCombo, cells);
+                    playerStatus.textContent = `Player ${current_player} wins`;
+                    isActive = false;
+                    resetBtn.style.display = 'inline-block';
+                    return
                 }
-            });
+
+                if(checkDraw()){
+                    playerStatus.textContent = "It's a Draw";
+                    isActive = false;
+                    resetBtn.style.display = 'inline-block';
+                    return
+                }
+                current_player = current_player === 'X'? 'O' : 'X';
+                playerStatus.textContent = `Player ${current_player}'s turn`;
+            }
         });
-        resetBtn.onclick = () => {
-            gameBoard.innerHTML = '';
-            board_values = ["","","","","","","","",""];
-            playerStatus.textContent = '';
-            playBtn.style.display = 'inline-block';
-            resetBtn.style.display = 'none'
-            multiplayerHuman()
-        }
+    });
+    resetBtn.onclick = () => {
+        resetGame()
+    }
+}
+// ########################################### Playing with Human end here ###################################################
+
+
+// ########################################### Playing with Computer start here ###################################################
+playComBtn.onclick = ()=>{
+    playComBtn.style.display = 'none';
+    playMulBtn.style.display = 'none';
+    multiplayerComputer()
+}
+let whosTurn = 'Your'
+function multiplayerComputer(){
+    playerStatus.textContent = "Your turn"
+    isActive = true;
+    for (let i = 0; i < 9; i++){
+        gameBoard.innerHTML += `<div class="cell" data-index=${i}></div>`
+    }
+    playerStatus.style.display = 'block';
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('click', ()=>{
+            index = parseInt(cell.getAttribute('data-index'))
+            if( isActive && board_values[index] == ""){
+                board_values[index] = current_player;
+                cell.textContent = current_player;
+
+                if (checkWinner()){
+                    winningCombo.forEach(combo => {
+                        cells[combo].classList.add('winning-cells')
+                    })
+                    drawLine(winningCombo, cells);
+                    playerStatus.textContent = `You win`;
+                    isActive = false;
+                    resetBtn.style.display = 'inline-block';
+                    return
+                }
+
+                if(checkDraw()){
+                    playerStatus.textContent = "It's a Draw";
+                    isActive = false;
+                    resetBtn.style.display = 'inline-block';
+                    return
+                }
+                whosTurn = whosTurn === 'Your'? "Computer's" : 'Your';
+                playerStatus.textContent = `${whosTurn} turn`;
+                setTimeout(()=>{
+                    computerTurn(cells)
+                },400);
+            }
+        });
+    });
+    resetBtn.onclick = () => {
+        resetGame()
     }
 }
 
+function computerTurn(cells){
+    let isEmpty = board_values.some(val => val == "");
+    input();
+    function input(){
+        if (!isEmpty){
+            return
+        }
+        setTimeout(()=>{
+            randomIndex = Math.floor(Math.random() * 9 )
+            if(board_values[randomIndex] == ""){
+                board_values[randomIndex] = "O";
+                cells[randomIndex].textContent = "O"
+
+                if (checkWinner()){
+                    winningCombo.forEach(combo => {
+                        cells[combo].classList.add('winning-cells')
+                    })
+                    drawLine(winningCombo, cells);
+                    playerStatus.textContent = `Computer Win`;
+                    isActive = false;
+                    resetBtn.style.display = 'inline-block';
+                    return
+                }
+            
+                if(checkDraw()){
+                    playerStatus.textContent = "It's a Draw";
+                    isActive = false;
+                    resetBtn.style.display = 'inline-block';
+                    return
+                }
+
+                isEmpty = false;
+                return
+            }
+            input()
+        },50)
+    }
+    whosTurn = whosTurn === 'Your'? "Computer's" : 'Your';
+    playerStatus.textContent = `${whosTurn} turn`;
+}
+// ########################################### Playing with Computer end here ###################################################
+
+// ########################################## necessary functions #############################################
 function checkWinner(){
-    
     return winningCombination.some(combo => {
         winningCombo = [...combo];
         let [a,b,c] = combo;
@@ -83,29 +177,26 @@ function checkDraw(){
     return board_values.every(val => val != "")
 };
 
+function resetGame(){
+    gameBoard.innerHTML = '';
+    board_values = ["","","","","","","","",""];
+    playerStatus.textContent = '';
+    playMulBtn.style.display = 'inline-block';
+    playComBtn.style.display = 'inline-block'
+    resetBtn.style.display = 'none'
+};
+
 function drawLine(combo, cells){
-    
-    if(combo[1] == 1){
-        cells[1].style.position = 'relative';
-        cells[1].innerHTML += `<div class='lineH'></div>`;
+    if(combo[1] == 1 || combo[1] == 7){
+        cells[combo[1]].style.position = 'relative';
+        cells[combo[1]].innerHTML += `<div class='lineH'></div>`;
         setTimeout(()=>{
             document.querySelector('.lineH').style.transform = 'scale(1)'
         },50)
-    }else if(combo[1] == 7){
-        cells[7].style.position = 'relative';
-        cells[7].innerHTML += `<div class='lineH'></div>`;
-        setTimeout(()=>{
-            document.querySelector('.lineH').style.transform = 'scale(1)'
-        },50)
-    }else if(combo[1] == 3){
-        cells[3].style.position = 'relative';
-        cells[3].innerHTML += `<div class='lineV'></div>`;
-        setTimeout(()=>{
-            document.querySelector('.lineV').style.transform = 'scale(1)'
-        },50)
-    }else if(combo[1] == 5){
-        cells[5].style.position = 'relative';
-        cells[5].innerHTML += `<div class='lineV'></div>`;
+    }
+    else if(combo[1] == 3 || combo[1] == 5){
+        cells[combo[1]].style.position = 'relative';
+        cells[combo[1]].innerHTML += `<div class='lineV'></div>`;
         setTimeout(()=>{
             document.querySelector('.lineV').style.transform = 'scale(1)'
         },50)
@@ -144,5 +235,3 @@ function drawLine(combo, cells){
         }
     }
 }
-
-multiplayerHuman();
